12f549430a22b7ec2a869f6ded3434d519b0843c
Jaroslaw Wencel <jaroslaw.wencel@gmail.com>
2025-02-14 23:47:16 +0100

    test

--- pipelines/python-simple.groovy
+++ pipelines/python-simple.groovy
@@ -2,17 +2,27 @@ pipeline {
     agent any
 
     stages {
+        stage('Checkout') {
+            steps {
+                checkout scmGit(userRemoteConfigs: [
+                    [ url: 'https://github.com/serafin-tech/python-scripts' ]
+                ])
+            }
+        }
         stage('Hello') {
             steps {
-                withPythonEnv('python3.11') {
-                    sh '''
-                        python3.11 -m pip install -U pip
-                        python3.11 -m pip install -U -r requirements.txt
-        
-                        python3.11 -m pip install -U build setuptools setuptools-scm
-        
-                        python3.11 -m build .
-                    '''
+                dir('python-scripts') {
+                    sh 'ls -l'
+                    withPythonEnv('python3.11') {
+                        sh '''
+                            python3.11 -m pip install -U pip
+                            python3.11 -m pip install -U -r requirements.txt
+            
+                            python3.11 -m pip install -U build setuptools setuptools-scm
+            
+                            python3.11 -m build .
+                        '''
+                    }
                 }
             }
         }
